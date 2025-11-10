f77ac37371d921d2a047b74be40d2f2534abebb3
ali <ali@iluk.com>
2025-11-10 17:05:00 -0600

    feat: create tasks page with full crud

---
+++ app/tasks/page.tsx
@@ -0,0 +1,33 @@
+'use client';
+import { useEffect } from 'react';
+import NewTask from './components/NewTask';
+import TasksBoard from './components/TasksBoard';
+import { useTaskStore } from '../stores/task-store';
+import { Spinner } from '@/components/ui/spinner';
+import Header from '@/components/Header';
+
+const LoadingSpinner = () => {
+  return (
+    <div className="flex justify-center w-screen">
+      <Spinner className="text-purple-500" />
+    </div>
+  );
+};
+
+export default function Tasks() {
+  const { fetchTasks, tasks } = useTaskStore();
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <div className="flex grow items-center justify-center pt-24 sm:pt-28 pb-6 font-sans dark:bg-black">
+        <NewTask />
+      </div>
+      {tasks.length === 0 ? <LoadingSpinner /> : <TasksBoard />}
+    </>
+  );
+}
